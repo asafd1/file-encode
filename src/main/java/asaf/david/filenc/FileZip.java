bd876b7ae6561fd427fe96f2cfc146bf2fa96b03
Asaf David <asaf.david@precise.com>
2014-08-26 12:59:13 +0300

    first commit

---
+++ src/main/java/asaf/david/filenc/FileZip.java
@@ -0,0 +1,250 @@
+package asaf.david.filenc;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.io.ZipOutputStream;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.util.Zip4jConstants;
+
+/**
+ * Hello world!
+ *
+ */
+public class FileZip
+{
+	enum ACTION
+	{
+		ENCRYPT, DECRYPT;
+	}
+
+	private ACTION m_action;
+	private File m_file;
+	private String m_password = null;
+
+	public static void main(String[] args)
+	{
+		FileZip fz = new FileZip();
+		fz.doWork(args);
+
+	}
+
+	private void doWork(String[] args)
+	{
+		readInput(args);
+
+		if (!m_file.exists())
+		{
+			errorFileNotFound();
+		}
+
+		act(m_file, m_action);
+	}
+
+	void act(File file, ACTION action)
+	{
+		if (file.isDirectory())
+		{
+			String[] files;
+
+			if (action == ACTION.ENCRYPT)
+			{
+				files = file.list((File dir, String name) -> {
+					return (name.indexOf("{z}") == -1);
+				});
+			}
+			else
+			{
+				files = file.list((File dir, String name) -> {
+					return (name.indexOf("{z}") != -1 || new File(dir, name).isDirectory());
+				});
+			}
+
+			for (String subFile : files)
+			{
+				act(new File(file, subFile), action);
+			}
+			return;
+		}
+
+		if (action == ACTION.ENCRYPT)
+		{
+			encryptFile(file);
+		}
+		else
+		{
+			decryptFile(file);
+		}
+	}
+
+	private void decryptFile(File file)
+	{
+		try
+		{
+			ZipFile zipFile = new ZipFile(file);
+			zipFile.setPassword(getPassword());
+			zipFile.extractAll(file.getParent());
+		}
+		catch (ZipException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private void encryptFile(File file)
+	{
+		ZipOutputStream outputStream = null;
+		InputStream inputStream = null;
+
+		try
+		{
+			// Initiate output stream with the path/file of the zip file
+			// Please note that ZipOutputStream will overwrite zip file if it already exists
+			outputStream = new ZipOutputStream(new FileOutputStream(new File(getZipFileName(file))));
+
+			ZipParameters parameters = new ZipParameters();
+
+			// Deflate compression or store(no compression) can be set below
+			parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
+
+			parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
+
+			parameters.setEncryptFiles(true);
+
+			parameters.setEncryptionMethod(Zip4jConstants.ENC_METHOD_AES);
+
+			parameters.setAesKeyStrength(Zip4jConstants.AES_STRENGTH_256);
+
+			parameters.setPassword(getPassword());
+
+			outputStream.putNextEntry(file, parameters);
+
+			inputStream = new FileInputStream(file);
+			byte[] readBuff = new byte[4096];
+			int readLen = -1;
+
+			while ((readLen = inputStream.read(readBuff)) != -1)
+			{
+				outputStream.write(readBuff, 0, readLen);
+			}
+
+			// Once the content of the file is copied, this entry to the zip file
+			// needs to be closed. ZipOutputStream updates necessary header information
+			// for this file in this step
+			outputStream.closeEntry();
+
+			inputStream.close();
+
+			// ZipOutputStream now writes zip header information to the zip file
+			outputStream.finish();
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (outputStream != null)
+			{
+				try
+				{
+					outputStream.close();
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+
+			if (inputStream != null)
+			{
+				try
+				{
+					inputStream.close();
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private String getZipFileName(File file)
+	{
+		return file.getAbsolutePath() + "{z}.zip";
+	}
+
+	private String getPassword()
+	{
+		if (m_password == null)
+		{
+			System.out.print("Enter password: ");
+			BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+			try
+			{
+				m_password = bufferRead.readLine();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+		return m_password;
+	}
+
+	private void readInput(String[] args)
+	{
+		if (args.length < 2)
+		{
+			errorWrongUsage();
+		}
+
+		if (args[0].equals("e"))
+		{
+			m_action = ACTION.ENCRYPT;
+		}
+		else if (args[0].equals("d"))
+		{
+			m_action = ACTION.DECRYPT;
+		}
+		else
+		{
+			errorWrongUsage();
+		}
+
+		m_file = new File(args[1]);
+	}
+
+	private void errorFileNotFound()
+	{
+		usage();
+		System.exit(-2);
+	}
+
+	private void errorWrongUsage()
+	{
+		usage();
+		System.exit(-2);
+	}
+
+	private void usage()
+	{
+		System.out.println("Usage: filenc e|d <file>|<dir>");
+
+	}
+
+	void setTestPass(String pass)
+	{
+		m_password = pass;
+	}
+}
---
+++ src/test/java/asaf/david/filenc/FileZipTest.java
@@ -0,0 +1,142 @@
+package asaf.david.filenc;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.nio.file.Files;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import asaf.david.filenc.FileZip.ACTION;
+
+/**
+ * Unit test for simple App.
+ */
+public class FileZipTest extends TestCase
+{
+	private static String RESOURCES_DIR = "src\\main\\resources";
+	private static File FILE1 = new File(RESOURCES_DIR, "a.txt");
+	private static File FILE2 = new File(RESOURCES_DIR, "\\f1\\b.txt");
+	private static File FILE3 = new File(RESOURCES_DIR, "\\f2\\a.txt");
+	private static File ENCRYPTED_FILE1 = new File(FILE1.getAbsoluteFile() + "{z}.zip");
+	private static File ENCRYPTED_FILE2 = new File(FILE2.getAbsoluteFile() + "{z}.zip");
+	private static File ENCRYPTED_FILE3 = new File(FILE3.getAbsoluteFile() + "{z}.zip");
+	private static String CONTENT1 = "content1";
+	private static String CONTENT2 = "content2";
+	private static String CONTENT3 = "content3";
+
+	public FileZipTest(String testName)
+	{
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite()
+	{
+		return new TestSuite(FileZipTest.class);
+	}
+
+	public void testEncodeSingleFile() throws Exception
+	{
+		FileZip fz = prepare();
+		fz.act(FILE1, ACTION.ENCRYPT);
+		assertTrue("coulnd't find resulting zip file", ENCRYPTED_FILE1.exists());
+	}
+
+	public void testEncodeFolder() throws Exception
+	{
+		FileZip fz = prepare();
+		fz.act(new File(RESOURCES_DIR), ACTION.ENCRYPT);
+		assertTrue("coulnd't find resulting zip file", ENCRYPTED_FILE1.exists());
+		assertTrue("coulnd't find resulting zip file", ENCRYPTED_FILE2.exists());
+		assertTrue("coulnd't find resulting zip file", ENCRYPTED_FILE3.exists());
+	}
+
+	public void testDecodeSingleFile() throws Exception
+	{
+		FileZip fz = prepare();
+		fz.act(FILE1, ACTION.ENCRYPT);
+		assertTrue("coulnd't find resulting zip file", ENCRYPTED_FILE1.exists());
+
+		Files.delete(FILE1.toPath());
+
+		fz.act(ENCRYPTED_FILE1, ACTION.DECRYPT);
+		assertTrue("coulnd't find extracted file", FILE1.exists());
+
+		String content = readFile(FILE1);
+		assertTrue("corrupt file after encode/decode", CONTENT1.equals(content));
+	}
+
+	public void testDecodeFolder() throws Exception
+	{
+		FileZip fz = prepare();
+		File f = new File(RESOURCES_DIR);
+		fz.act(f, ACTION.ENCRYPT);
+
+		Files.delete(FILE1.toPath());
+		Files.delete(FILE2.toPath());
+		Files.delete(FILE3.toPath());
+
+		fz.act(f, ACTION.DECRYPT);
+		assertTrue("coulnd't find extracted file", ENCRYPTED_FILE1.exists());
+		assertTrue("coulnd't find extracted file", ENCRYPTED_FILE2.exists());
+		assertTrue("coulnd't find extracted file", ENCRYPTED_FILE3.exists());
+
+		String content1 = readFile(FILE1);
+		String content2 = readFile(FILE2);
+		String content3 = readFile(FILE3);
+		assertTrue("corrupt file after encode/decode", CONTENT1.equals(content1));
+		assertTrue("corrupt file after encode/decode", CONTENT2.equals(content2));
+		assertTrue("corrupt file after encode/decode", CONTENT3.equals(content3));
+	}
+
+	private FileZip prepare() throws Exception
+	{
+		deleteFile(new File(RESOURCES_DIR));
+		FILE1.getParentFile().mkdir();
+		FILE2.getParentFile().mkdir();
+		FILE3.getParentFile().mkdir();
+
+		createFile(FILE1, CONTENT1);
+		createFile(FILE2, CONTENT2);
+		createFile(FILE3, CONTENT3);
+
+		FileZip fz = new FileZip();
+		fz.setTestPass("pass");
+		return fz;
+	}
+
+	private void deleteFile(File file) throws Exception
+	{
+		if (file.isDirectory())
+		{
+			String[] files = file.list();
+			for (String subFile : files)
+			{
+				deleteFile(new File(file, subFile));
+			}
+		}
+
+		file.delete();
+	}
+
+	private void createFile(File file, String content) throws Exception
+	{
+		PrintStream stream = new PrintStream(file);
+		stream.append(content);
+		stream.flush();
+		stream.close();
+	}
+
+	private String readFile(File file) throws Exception
+	{
+		char[] buff = new char[1024];
+		FileReader reader = new FileReader(file);
+		int n = reader.read(buff);
+		reader.close();
+		return String.valueOf(buff).substring(0, n);
+	}
+}
